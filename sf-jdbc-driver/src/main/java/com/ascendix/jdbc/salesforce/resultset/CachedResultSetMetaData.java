c23054b4200d076ac04b36c196062b0c9f363965
Luca Rota <lrota@eutelsat.com>
2024-02-28 10:20:29 +0100

    Update lib versions

--- sf-jdbc-driver/src/main/java/com/ascendix/jdbc/salesforce/resultset/CachedResultSetMetaData.java
+++ sf-jdbc-driver/src/main/java/com/ascendix/jdbc/salesforce/resultset/CachedResultSetMetaData.java
@@ -7,121 +7,118 @@ public class CachedResultSetMetaData implements ResultSetMetaData {
 
     public final static CachedResultSetMetaData EMPTY = new CachedResultSetMetaData();
 
+    @Override
+    public int getColumnCount() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public boolean isAutoIncrement(final int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isCaseSensitive(final int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isSearchable(final int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isCurrency(final int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public int isNullable(final int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public boolean isSigned(final int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public int getColumnDisplaySize(final int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getColumnLabel(final int column) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getColumnName(final int column) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getSchemaName(final int column) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int getPrecision(final int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int getScale(final int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getTableName(final int column) throws SQLException {
+        return null;
+    }
+
+    @Override
     public String getCatalogName(int column) throws SQLException {
         return "";
     }
 
-    //
-    // Not implemented below here
-    //
-
-    public String getColumnClassName(int column) throws SQLException {
-
-        return null;
-    }
-
-    public int getColumnCount() throws SQLException {
-
+    @Override
+    public int getColumnType(final int column) throws SQLException {
         return 0;
     }
 
-    public int getColumnDisplaySize(int column) throws SQLException {
-
-        return 0;
-    }
-
-    public String getColumnLabel(int column) throws SQLException {
-
+    @Override
+    public String getColumnTypeName(final int column) throws SQLException {
         return null;
     }
 
-    public String getColumnName(int column) throws SQLException {
+    @Override
+    public boolean isReadOnly(final int column) throws SQLException {
+        return false;
+    }
 
+    @Override
+    public boolean isWritable(final int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isDefinitelyWritable(final int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public String getColumnClassName(final int column) throws SQLException {
         return null;
     }
 
-    public int getColumnType(int column) throws SQLException {
-
-        return 0;
-    }
-
-    public String getColumnTypeName(int column) throws SQLException {
-
+    @Override
+    public <T> T unwrap(final Class<T> iface) throws SQLException {
         return null;
     }
 
-    public int getPrecision(int column) throws SQLException {
-
-        return 0;
-    }
-
-    public int getScale(int column) throws SQLException {
-
-        return 0;
-    }
-
-    public String getSchemaName(int column) throws SQLException {
-
-        return null;
-    }
-
-    public String getTableName(int column) throws SQLException {
-
-        return null;
-    }
-
-    public boolean isAutoIncrement(int column) throws SQLException {
-
-        return false;
-    }
-
-    public boolean isCaseSensitive(int column) throws SQLException {
-
-        return false;
-    }
-
-    public boolean isCurrency(int column) throws SQLException {
-
-        return false;
-    }
-
-    public boolean isDefinitelyWritable(int column) throws SQLException {
-
-        return false;
-    }
-
-    public int isNullable(int column) throws SQLException {
-
-        return 0;
-    }
-
-    public boolean isReadOnly(int column) throws SQLException {
-
-        return false;
-    }
-
-    public boolean isSearchable(int column) throws SQLException {
-
-        return false;
-    }
-
-    public boolean isSigned(int column) throws SQLException {
-
-        return false;
-    }
-
-    public boolean isWritable(int column) throws SQLException {
-
-        return false;
-    }
-
-    public <T> T unwrap(Class<T> iface) throws SQLException {
-
-        return null;
-    }
-
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-
+    @Override
+    public boolean isWrapperFor(final Class<?> iface) throws SQLException {
         return false;
     }
 }
