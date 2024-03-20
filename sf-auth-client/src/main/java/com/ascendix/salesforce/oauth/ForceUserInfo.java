fdb90e92a0c81978c459a5cb86de4aeca61b3080
Luca Rota <lrota@eutelsat.com>
2024-03-20 18:42:32 +0100

    Refactoring sf-auth-client to use only jre method (written a basic json parser for validator) and removed some dependencies

--- sf-auth-client/src/main/java/com/ascendix/salesforce/oauth/ForceUserInfo.java
+++ sf-auth-client/src/main/java/com/ascendix/salesforce/oauth/ForceUserInfo.java
@@ -1,6 +1,5 @@
 package com.ascendix.salesforce.oauth;
 
-import com.google.api.client.util.Key;
 import java.util.Map;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,22 +8,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ForceUserInfo {
 
-    @Key("user_id")
     private String userId;
-    @Key("organization_id")
     private String organizationId;
-    @Key("preferred_username")
     private String preferredUsername;
-    @Key("nickname")
     private String nickName;
     private String name;
     private String email;
-    @Key("zoneinfo")
     private String timeZone;
-    @Key("locale")
     private String locale;
     private String instance;
     private String partnerUrl;
-    @Key("urls")
     private Map<String, String> urls;
 }
