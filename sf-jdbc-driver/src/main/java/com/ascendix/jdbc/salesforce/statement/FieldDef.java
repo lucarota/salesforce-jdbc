9cb9ebde7beedb6f00fb207dc2804f796b24bc88
Luca Rota <lrota@eutelsat.com>
2024-03-21 13:37:36 +0100

    Fix typo in test's file name

--- sf-jdbc-driver/src/main/java/com/ascendix/jdbc/salesforce/statement/FieldDef.java
+++ sf-jdbc-driver/src/main/java/com/ascendix/jdbc/salesforce/statement/FieldDef.java
@@ -1,5 +1,10 @@
 package com.ascendix.jdbc.salesforce.statement;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
 public class FieldDef {
 
     /**
@@ -7,7 +12,7 @@ public class FieldDef {
      */
     private final String name;
     /**
-     * Full bname of the field with sub entity or name of the aggregation function like:
+     * Full name of the field with sub entity or name of the aggregation function like:
      * 1) Owner.Name  for select Owner.Name from Account
      * 2) maxLastName for MAX(LastName)
      */
@@ -18,51 +23,5 @@ public class FieldDef {
         this.name = name;
         this.alias = alias;
         this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((type == null) ? 0 : type.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        FieldDef other = (FieldDef) obj;
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        if (type == null) {
-            return other.type == null;
-        } else
-            return type.equals(other.type);
     }
 }
