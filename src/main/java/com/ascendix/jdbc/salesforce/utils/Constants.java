03bb9c472d686f890d95eefe8863c3ecd4300b62
Luca Rota <luca.rota@live.it>
2024-11-27 16:57:40 +0100

    Fix query with boolean value (needs JSQLParser 5.1-SNAPSHOT until the release of version 5.1)

--- src/main/java/com/ascendix/jdbc/salesforce/utils/Constants.java
+++ src/main/java/com/ascendix/jdbc/salesforce/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
     public static final int DRIVER_MAJOR_VER = 1;
     public static final int DRIVER_MINOR_VER = 6;
-    public static final int DRIVER_REVISION_VER = 12;
+    public static final int DRIVER_REVISION_VER = 13;
 
     public static final String DRIVER_VERSION = DRIVER_MAJOR_VER + "." + DRIVER_MINOR_VER + "." + DRIVER_REVISION_VER;
 
